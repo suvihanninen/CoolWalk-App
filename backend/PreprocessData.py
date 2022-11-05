0e85b58a18aff7e1f7848d22fb668807d7462d63
Kris Otte <krot@itu.dk>
2022-11-05 17:57:16 +0100

    deleted read_data.ipynb class

---
+++ backend/PreprocessData.py
@@ -0,0 +1,15 @@
+# import modules
+import geopandas as gpd
+import matplotlib.pyplot as plt
+from shapely import wkt
+from shapely.geometry import Point
+import shapely
+import osmnx as ox
+import pandas as pd
+import networkx as nx
+from shapely.geometry import LineString
+from tabulate import tabulate
+from IPython.display import display
+
+class PreprocessData:
+    __nodesPd:pd.DataFrame = readData.ReadData().get_nodes_as_pd()
