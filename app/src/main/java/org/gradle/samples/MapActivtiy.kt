d2067a985c72d22861d74bb49848305a6190ef3f
“Suvi <suvh@itu.dk>
2022-11-05 10:00:43 +0100

    Added line functionality

--- app/src/main/java/org/gradle/samples/MapActivtiy.kt
+++ app/src/main/java/org/gradle/samples/MapActivtiy.kt
@@ -1,41 +1,56 @@
 package org.gradle.samples
 
 
+
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
-import org.gradle.samples.R
 
 
-
-class MapActivtiy : AppCompatActivity() {
+class MapActivtiy:AppCompatActivity() {
     var mapView: MapView? = null
-
+    lateinit var btn_submit: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+        btn_submit = findViewById(R.id.button)
         mapView = findViewById(R.id.mapView)
-        mapView?.getMapboxMap()?.loadStyleUri(Style.OUTDOORS, object : Style.OnStyleLoaded {
+        mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS, object : Style.OnStyleLoaded {
             override fun onStyleLoaded(style: Style) {
-                println("HEY YOU WHATS UP")
-            }})
+
+            }
+        })
+
+        btn_submit.setOnClickListener {
+
+            val intent = Intent(this, DrawGeoJsonLineActivity::class.java)
+            startActivity(intent)
+        }
 
 
     }
 
-    override fun onStart() {
+
+
+    override fun onStart()
+    {
         super.onStart()
         mapView?.onStart()
     }
 
-    override fun onStop() {
+    override fun onStop()
+    {
         super.onStop()
         mapView?.onStop()
     }
 
-    override fun onLowMemory() {
+    override fun onLowMemory()
+    {
         super.onLowMemory()
         mapView?.onLowMemory()
     }
@@ -43,5 +58,5 @@ class MapActivtiy : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         mapView?.onDestroy()
-    }
-}
+    }}
+
