ed4d57f5d02d53c4921226b0b9023701dd194998
Kris Otte <krot@itu.dk>
2022-11-05 17:38:22 +0100

    set up ReadData class

--- backend/ReadData.py
+++ backend/ReadData.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
 # import modules
 import geopandas as gpd
 import matplotlib.pyplot as plt
@@ -13,23 +9,22 @@ import pandas as pd
 import networkx as nx
 from shapely.geometry import LineString
 from tabulate import tabulate
+from IPython.display import display
 
 
 """
-    Class responsible for reading data from CSV file (TO-DO: make it read from PostGis data base) and return it as pandas dataframes
+    Class responsible for reading data from CSV file (TODO: make it read from PostGis data base) and return it as pandas dataframes
 
 """
-class ReadData(object):
-    """Creating class attrributes which can only be accessed via Class reference"""
+class ReadData():
+    __dfNodes = pd.read_csv("/mnt/c/users/kriso/documents/itu/5th_semester/research_project/CoolWalk/data/CoolWalksDataStorge/nodes.csv")
+    __dfEdges = pd.read_csv("/mnt/c/users/kriso/documents/itu/5th_semester/research_project/CoolWalk/data/CoolWalksDataStorge/edges.csv")
 
-    dfNodes = pd.read_csv("/mnt/c/users/kriso/documents/itu/5th_semester/research_project/CoolWalk/data/CoolWalksDataStorge/nodes.csv")
-    dfEdges = pd.read_csv("/mnt/c/users/kriso/documents/itu/5th_semester/research_project/CoolWalk/data/CoolWalksDataStorge/edges.csv")
-    """print(dfNodes)"""
 
-    def __init__(self):
-        self.nodes = pd.read_csv("/mnt/c/users/kriso/documents/itu/5th_semester/research_project/CoolWalk/data/CoolWalksDataStorge/nodes.csv")
-        self.edges = pd.read_csv("/mnt/c/users/kriso/documents/itu/5th_semester/research_project/CoolWalk/data/CoolWalksDataStorge/edges.csv")
+    @staticmethod
+    def get_nodes_as_pd() -> pd.DataFrame:
+        return ReadData.__dfNodes
 
-    def someFunction():
-        return "hello"
-
+    @staticmethod
+    def get_edges_as_pd() -> pd.DataFrame:
+        return ReadData.__dfEdges
