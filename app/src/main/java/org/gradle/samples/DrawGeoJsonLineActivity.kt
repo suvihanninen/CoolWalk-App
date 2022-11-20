ba485cc6a5c3e9cdfeae3efc6fd7fe695346d241
“Suvi <suvh@itu.dk>
2022-11-20 10:35:56 +0100

    draws the line from a feature collection

--- app/src/main/java/org/gradle/samples/DrawGeoJsonLineActivity.kt
+++ app/src/main/java/org/gradle/samples/DrawGeoJsonLineActivity.kt
@@ -24,19 +24,12 @@ import com.mapbox.maps.extension.style.style
 //code resource: https://docs.mapbox.com/android/maps/examples/draw-geo-json-line/
 class DrawGeoJsonLineActivity : AppCompatActivity() {
     var mapView:MapView?=null
-    var showLineBtn:Button?=null
-    var sendBtn:Button?=null
-    val routeCoordinates=mutableListOf<Point>()
 
     public override fun onCreate(savedInstanceState:Bundle?){
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
+        setContentView(R.layout.activity_draw_geo_json_line)
         mapView=findViewById(R.id.mapView)
-        showLineBtn=findViewById(R.id.getDataButton)
-        sendBtn=findViewById(R.id.sendDataButton)
-        showLineBtn?.setVisibility(View.INVISIBLE)
-        sendBtn?.setVisibility(View.INVISIBLE)
-        initRouteCoordinates()
+
         mapView?.getMapboxMap()?.setCamera(
             CameraOptions.Builder().center(
                 Point.fromLngLat(
@@ -49,34 +42,18 @@ class DrawGeoJsonLineActivity : AppCompatActivity() {
             (
                     style(styleUri = Style.MAPBOX_STREETS) {
                         +geoJsonSource(DrawGeoJsonLineActivity.GEOJSON_SOURCE_ID) {
-                            featureCollection(featureCollection)
+                            featureCollection(PointListSingleton.featureCollection)
                         }
                         +lineLayer("linelayer", DrawGeoJsonLineActivity.GEOJSON_SOURCE_ID) {
                             lineCap(LineCap.ROUND)
                             lineJoin(LineJoin.ROUND)
                             lineOpacity(0.9999)
-                            lineWidth(10.0)
+                            lineWidth(2.0)
                             lineColor("#FF5733")
                         }
                     }
                     )
         )
-    }
-
-
-    var featureLineString = Feature.fromGeometry(LineString.fromLngLats(routeCoordinates))
-    var featureCollection = FeatureCollection.fromFeature(featureLineString)
-
-
-    fun initRouteCoordinates() {
-        // Create a list to store our line coordinates.
-
-        routeCoordinates.add(Point.fromLngLat(-1.230805, 52.917281))
-        routeCoordinates.add(Point.fromLngLat(-1.230408, 52.917191))
-        routeCoordinates.add(Point.fromLngLat(-1.231129, 52.916005))
-        routeCoordinates.add(Point.fromLngLat(-1.231455, 52.915496))
-        routeCoordinates.add(Point.fromLngLat(-1.231467,  52.915294))
-
     }
 
     companion object {
